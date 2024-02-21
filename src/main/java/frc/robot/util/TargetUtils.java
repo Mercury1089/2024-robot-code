afabb32b7f9bdda0a529abd377f806fc6b95cd16
Rohan Shah <57906961+rshah713@users.noreply.github.com>
2024-02-21 16:42:23 -0500

    Simplified getTargetHeadingToFieldPosition method

--- src/main/java/frc/robot/util/TargetUtils.java
+++ src/main/java/frc/robot/util/TargetUtils.java
@@ -33,28 +33,20 @@ public class TargetUtils {
         Optional<Alliance> allianceColor = DriverStation.getAlliance();
         Optional<Pose3d> tagPose = Optional.empty();
         
-        if (fieldPos == FieldPosition.SPEAKER) {
-            tagPose = (allianceColor.get() == Alliance.Blue) ? 
-                photonCam.getTagPose(APRILTAGS.MIDDLE_BLUE_SPEAKER) : 
-                photonCam.getTagPose(APRILTAGS.MIDDLE_RED_SPEAKER);
-        } else if (fieldPos == FieldPosition.AMP) {
-            tagPose = (allianceColor.get() == Alliance.Blue) && fieldPos == FieldPosition.AMP ? 
-                photonCam.getTagPose(APRILTAGS.BLUE_AMP) : 
-                photonCam.getTagPose(APRILTAGS.RED_AMP);
-        }
-
-        if (fieldPos == FieldPosition.SPEAKER && tagPose.isPresent()) {
+        if (tagPose.isPresent() && fieldPos == FieldPosition.SPEAKER) {
             int apriltag = (allianceColor.get() == Alliance.Blue) ?
                 APRILTAGS.MIDDLE_BLUE_SPEAKER :
                 APRILTAGS.MIDDLE_RED_SPEAKER;
-
+            tagPose = photonCam.getTagPose(apriltag);
+            
             return robotPose.getY() < tagPose.get().getTranslation().getY() ? 
                 -Math.acos((robotPose.getTranslation().getX() - tagPose.get().getTranslation().getX()) / getDistanceToFieldPos(photonCam, robotPose, apriltag)) * (180 / Math.PI) :
                 Math.acos((robotPose.getTranslation().getX() - tagPose.get().getTranslation().getX()) / getDistanceToFieldPos(photonCam, robotPose, apriltag)) * (180 / Math.PI);
-        } else if (fieldPos == FieldPosition.AMP && tagPose.isPresent()) {
+        } else if (fieldPos == FieldPosition.AMP) {
             int apriltag = (allianceColor.get() == Alliance.Blue) ?
                 APRILTAGS.BLUE_AMP :
                 APRILTAGS.RED_AMP;
+            tagPose = photonCam.getTagPose(apriltag);
 
             return robotPose.getX() < tagPose.get().getTranslation().getX() ? 
                 Math.asin((tagPose.get().getTranslation().getY() - robotPose.getTranslation().getY()) / getDistanceToFieldPos(photonCam, robotPose, apriltag)) * (180 / Math.PI) - 180 :
